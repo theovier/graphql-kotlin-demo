25a592930b7b2accfee2dbe8bbe573841b191d4f
theovier <Theo@Harkenbusch.com>
2021-03-20 11:40:23 +0100

    add extremely basic db init methods

---
+++ src/main/kotlin/com/example/demo/DBInitializer.kt
@@ -0,0 +1,25 @@
+package com.example.demo
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import io.r2dbc.spi.ConnectionFactory
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.Bean
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
+
+@Configuration
+class DBInitializer {
+
+    @Bean
+    fun initializer(@Qualifier("connectionFactory") connectionFactory: ConnectionFactory): ConnectionFactoryInitializer {
+        return ConnectionFactoryInitializer().apply {
+            setConnectionFactory(connectionFactory)
+            setDatabasePopulator(CompositeDatabasePopulator().apply {
+                addPopulators(ResourceDatabasePopulator(ClassPathResource("/migrations/schema.sql")))
+                addPopulators(ResourceDatabasePopulator(ClassPathResource("/seeder/employee.sql")))
+            })
+        }
+    }
+}
